6fbcb8f2616a0c4fa827f69b85e76e1d34e3a400
Dhruuuuuuv <dhruvkumar787898@gmail.com>
2025-11-27 19:45:57 +0530

    Add files via upload

---
+++ day14_q1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n, sum = 0;
+
+    scanf("%d", &n);
+
+    for(int i = 1; i <= 2*n-1; i += 2) {
+        sum += i;
+    }
+
+    printf("%d\n", sum);
+
+    return 0;
+}
+
