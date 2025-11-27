fc9b8c105f35f78ec100829257f63ed6764f75b4
Dhruuuuuuv <dhruvkumar787898@gmail.com>
2025-11-27 19:48:41 +0530

    Add files via upload

---
+++ day20_q1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n, remainder, product = 1, hasOdd = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while(n != 0) {
+        remainder = n % 10;
+        if(remainder % 2 != 0) {
+            product *= remainder;
+            hasOdd = 1;
+        }
+        n /= 10;
+    }
+
+    if(hasOdd)
+        printf("Product of odd digits = %d\n", product);
+    else
+        printf("No odd digits in the number.\n");
+
+    return 0;
+}
