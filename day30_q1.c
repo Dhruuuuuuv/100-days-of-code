2319e69d0227aafc0604a203557fb463ab9a66f6
Dhruuuuuuv <dhruvkumar787898@gmail.com>
2025-11-27 20:47:23 +0530

    Add files via upload

---
+++ day30_q1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n, evenCount = 0, oddCount = 0;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for(int i = 0; i < n; i++) {
+        printf("Enter element %d: ", i+1);
+        scanf("%d", &arr[i]);
+        if(arr[i] % 2 == 0)
+            evenCount++;
+        else
+            oddCount++;
+    }
+
+    printf("Number of even elements = %d\n", evenCount);
+    printf("Number of odd elements = %d\n", oddCount);
+
+    return 0;
+}
